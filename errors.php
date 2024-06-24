048197f41de6eb45c1d54c5857e250fbdb3d18f9
hamdaouiwassim <hamdaouiwassim@gmil.com>
2024-06-24 20:14:21 +0200

    Types of errors

---
+++ errors.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1); // show | hide errors
+// Parse or syntax errors
+
+$a = 10;
+echo $a;
+
+// fatal errors
+
+//echo add(10,40);
+
+// Warning error
+
+include "file.php";
+
+// Notice errors
+
+$a = $b;
+
+
+?>
