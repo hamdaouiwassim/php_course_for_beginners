1b724a3c3cb9d2d490516064c49bb34277bdc7ba
hamdaouiwassim <hamdaouiwassim@gmil.com>
2024-06-25 12:04:06 +0200

    indexed arrays

---
+++ arrays.php
@@ -0,0 +1,30 @@
+<?php
+
+// arrays
+
+// indexed arrays
+
+$cars = array( "BMW" , "AUDI" , "Mercedes" , "Fiat" );
+
+// ( key | index ) => value 
+
+//echo $cars[3];
+
+//echo $cars;
+
+var_dump($cars);
+
+echo "tableau contient ==> " . count( $cars );
+
+$numbers = array( 1, 2, 30 , -9 , 0  );
+
+var_dump($numbers);
+
+echo count( $numbers );
+
+
+
+
+
+
+?>
